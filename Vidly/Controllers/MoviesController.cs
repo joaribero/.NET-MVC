812da076fd76fc660a55f8c4a765d8efc3c09a05
Joa Ribero <joaribero_la12@hotmail.com>
2018-03-01 17:08:06 -0300

    Primer commit
    Creando controlador y viendo cosas basicas.

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek"};
+            
+            return View(movie);
+        }
+
+        //public ActionResult Edit (int id)
+        //{
+        //    return Content("id=" + id);
+        //}
+
+        //public ActionResult Index (int? pageIndex, string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+        //        pageIndex = 1;
+        //    if (String.IsNullOrWhiteSpace(sortBy))
+        //        sortBy = "Name";
+
+        //    return Content(String.Format("pageIndex={0}&sortBy{1}", pageIndex, sortBy));
+        //}
+
+        [Route("Movies/Release/{year}/{month: regex(\\d{2}): range(1,12) }")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
