1bc7f6a313e2f104eb2d9364eb22d7c1f1b4999a
Joaribero <joaribero_la12@hotmail.com>
2019-09-17 19:23:32 -0300

    add Mapping customerDto and camel notations

--- Vidly/App_Start/WebApiConfig.cs
+++ Vidly/App_Start/WebApiConfig.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using Newtonsoft.Json;
 
 namespace Vidly
 {
@@ -9,6 +11,10 @@ namespace Vidly
     {
         public static void Register(HttpConfiguration config)
         {
+            var settings = config.Formatters.JsonFormatter.SerializerSettings;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            settings.Formatting = Formatting.Indented;
+
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
---
+++ Vidly/Dtos/CustomerDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter customer's name.")]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSuscribedToNewsLetter { get; set; }      
+
+        [Required]
+        public byte MembershipTypeId { get; set; }
+
+        [Min18YearsIfAMember]
+        public DateTime? Birthdate { get; set; }
+    }
+}
